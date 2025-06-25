ed7906bd1f902623ce24f4730d944428d88fa00a
LlamaLad7 <l3gomindstorms@gmail.com>
2025-06-25 13:16:59 +0100

    New: Misc support for IDE plugin.

--- annotations/src/main/java/com/llamalad7/mixintests/ap/MixinTestGenerator.java
+++ annotations/src/main/java/com/llamalad7/mixintests/ap/MixinTestGenerator.java
@@ -1,21 +1,20 @@
 package com.llamalad7.mixintests.ap;
 
+import com.llamalad7.mixintests.ap.annotations.TestLocation;
 import com.palantir.javapoet.*;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
 import javax.annotation.processing.Filer;
 import javax.lang.model.element.Modifier;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
 public record MixinTestGenerator(List<MixinTestConfig> configs) {
     private static final String TESTS_PACKAGE = "com.llamalad7.mixintests.tests";
-    private static final String MIXINS_PACKAGE = TESTS_PACKAGE + ".mixins";
     private static final ClassName TEST_BOOTSTRAP = ClassName.get("com.llamalad7.mixintests.harness", "TestBootstrap");
 
     public void generate(Filer filer) {
@@ -44,23 +43,34 @@ public record MixinTestGenerator(List<MixinTestConfig> configs) {
     }
 
     private MethodSpec generateTest(MixinTestConfig config) {
+        ClassName testName = ClassName.get(config.getTestClass());
         return MethodSpec.methodBuilder(testMethodName(config.getTestName()))
                 .returns(ParameterizedTypeName.get(Stream.class, DynamicTest.class))
                 .addModifiers(Modifier.PUBLIC)
                 .addAnnotation(TestFactory.class)
+                .addAnnotation(
+                        AnnotationSpec.builder(TestLocation.class)
+                                .addMember("value", "$T.class", testName)
+                                .build()
+                )
+                .addAnnotation(
+                        AnnotationSpec.builder(DisplayName.class)
+                                .addMember("value", "$S", config.getTestName())
+                                .build()
+                )
                 .addCode(
                         "return $T.doTest($S, $S, $T.class);",
                         TEST_BOOTSTRAP,
                         config.getTestName(),
                         config.getFileName(),
-                        ClassName.get(config.getTestClass())
+                        testName
                 )
                 .build();
     }
 
     private String testMethodName(String testName) {
         String[] parts = testName.split("\\.");
-        return "test" + String.join("", Arrays.stream(parts).map(StringUtils::capitalize).toList());
+        return "test$" + String.join("$", parts);
     }
 
     private MethodSpec generateAfterTestsMethod() {
---
+++ annotations/src/main/java/com/llamalad7/mixintests/ap/annotations/TestLocation.java
@@ -0,0 +1,5 @@
+package com.llamalad7.mixintests.ap.annotations;
+
+public @interface TestLocation {
+    Class<?> value();
+}
