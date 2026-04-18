e7a84e6645cc865953c80fe0f4abb89523fcc218
LlamaLad7 <l3gomindstorms@gmail.com>
2026-04-18 17:03:03 +0100

    Build: FabricMixin 0.17.3

--- buildSrc/src/main/kotlin/artifactVersions.kt
+++ buildSrc/src/main/kotlin/artifactVersions.kt
@@ -10,7 +10,7 @@ val FABRIC_MIXIN_VERSIONS = listOf(
     "0.10.7+mixin.0.8.4",
     "0.13.4+mixin.0.8.5",
     "0.15.5+mixin.0.8.7",
-    "0.17.2+mixin.0.8.7",
+    "0.17.3+mixin.0.8.7",
 )
 
 val MIXINEXTRAS_VERSIONS = listOf(
