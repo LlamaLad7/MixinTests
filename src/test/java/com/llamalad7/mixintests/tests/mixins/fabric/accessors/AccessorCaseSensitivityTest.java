ffa73e6e2817204a5cf1ce6fa8c9f215a847454f
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2026-07-11 14:34:14 -0700

    Add accessor case sensitivity test

---
+++ src/test/java/com/llamalad7/mixintests/tests/mixins/fabric/accessors/AccessorCaseSensitivityTest.java
@@ -0,0 +1,29 @@
+package com.llamalad7.mixintests.tests.mixins.fabric.accessors;
+
+import com.llamalad7.mixintests.ap.annotations.MixinTest;
+import com.llamalad7.mixintests.ap.annotations.TestOption;
+import com.llamalad7.mixintests.harness.tests.TestBox;
+import com.llamalad7.mixintests.tests.targets.AccessorTarget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@MixinTest(box = AccessorTarget.class, minFabricMixin = "0.17.4", fabricMixin = TestOption.ON)
+public class AccessorCaseSensitivityTest {
+    @Mixin(AccessorTarget.class)
+    static abstract class TestMixin extends TestBox {
+        @Accessor
+        abstract String getTest();
+
+        @Accessor
+        abstract String getTEST();
+
+        @Inject(method = "box", at = @At("HEAD"))
+        private void box(CallbackInfo ci) {
+            print("test: " + getTest());
+            print("TEST: " + getTEST());
+        }
+    }
+}
