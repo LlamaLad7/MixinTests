2f35fc37c1514bdb5bc05851669a76c363979aed
LlamaLad7 <l3gomindstorms@gmail.com>
2026-07-14 20:47:48 +0100

    New: Add/rename tests for lack of Accessor and Invoker conflicts.

--- src/test/java/com/llamalad7/mixintests/tests/mixins/fabric/accessors/NoInvokerConflicts.java
+++ src/test/java/com/llamalad7/mixintests/tests/mixins/fabric/accessors/NoInvokerConflicts.java
@@ -3,34 +3,34 @@ package com.llamalad7.mixintests.tests.mixins.fabric.accessors;
 import com.llamalad7.mixintests.ap.annotations.MixinTest;
 import com.llamalad7.mixintests.ap.annotations.TestOption;
 import com.llamalad7.mixintests.harness.tests.TestBox;
-import com.llamalad7.mixintests.tests.targets.AccessorTarget;
+import com.llamalad7.mixintests.tests.targets.InvokerTarget;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@MixinTest(box = AccessorTarget.class, minFabricMixin = "0.17.4", fabricMixin = TestOption.ON)
-public class AccessorSingleLetterTest {
-    @Mixin(AccessorTarget.class)
+@MixinTest(box = InvokerTarget.class, minFabricMixin = "0.17.4", fabricMixin = TestOption.ON)
+public class NoInvokerConflicts {
+    @Mixin(InvokerTarget.class)
     static abstract class Mixin1 extends TestBox {
-        @Accessor("x")
-        abstract String getX();
+        @Invoker("x")
+        abstract String callX();
 
         @Inject(method = "box", at = @At("HEAD"))
         private void box(CallbackInfo ci) {
-            print("x: " + getX());
+            print("x: " + callX());
         }
     }
 
-    @Mixin(AccessorTarget.class)
+    @Mixin(InvokerTarget.class)
     static abstract class Mixin2 extends TestBox {
-        @Accessor
-        abstract String getX();
+        @Invoker
+        abstract String callX();
 
         @Inject(method = "box", at = @At("HEAD"))
         private void box(CallbackInfo ci) {
-            print("x: " + getX());
+            print("x: " + callX());
         }
     }
 }
