25e6df99ff1091858e9ad8b1d3d74fc8e6b561e4
CelDaemon <celdaemon@voidgroup.net>
2026-08-01 13:58:36 +0200

    Add test to check accessor visibility

---
+++ src/test/java/com/llamalad7/mixintests/tests/mixins/fabric/accessors/VisibleAccessors.java
@@ -0,0 +1,37 @@
+package com.llamalad7.mixintests.tests.mixins.fabric.accessors;
+
+import com.llamalad7.mixintests.ap.annotations.MixinTest;
+import com.llamalad7.mixintests.ap.annotations.TestOption;
+import com.llamalad7.mixintests.harness.tests.TestBox;
+import com.llamalad7.mixintests.tests.targets.AccessorTarget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.lang.reflect.Method;
+
+@MixinTest(box = AccessorTarget.class, fabricMixin = TestOption.ON)
+public class VisibleAccessors {
+    @Mixin(AccessorTarget.class)
+    static abstract class Mixin1 extends TestBox {
+        @Accessor("x")
+        abstract String getX();
+
+        @Accessor("test")
+        public abstract String getTest();
+
+        @Inject(method = "box", at = @At("HEAD"))
+        private void box(CallbackInfo ci) throws NoSuchMethodException {
+            Method[] methods = new Method[] {
+                    Mixin1.class.getDeclaredMethod("getX"),
+                    Mixin1.class.getDeclaredMethod("getTest")
+            };
+
+            for (Method method : methods) {
+                print(method + ": " + String.format("%04x", method.getModifiers()));
+            }
+        }
+    }
+}
