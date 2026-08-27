6b4c4b3799453495a7116a68709770dc7ad9aa84
LlamaLad7 <l3gomindstorms@gmail.com>
2026-08-27 17:50:24 +0100

    Build: Compile against local versions if applicable.

--- buildSrc/src/main/kotlin/artifactVersions.kt
+++ buildSrc/src/main/kotlin/artifactVersions.kt
@@ -1,3 +1,5 @@
+import org.gradle.api.Project
+
 const val LOCAL_FABRIC_MIXIN_VERSION = "1.0.0+mixin.1.0.0"
 const val LOCAL_MIXINEXTRAS_VERSION = "1.0.0"
 
@@ -7,16 +9,18 @@ val MIXIN_VERSIONS = listOf(
     "0.8.7",
 )
 
-val FABRIC_MIXIN_VERSIONS = listOf(
-    "0.10.7+mixin.0.8.4",
-    "0.13.4+mixin.0.8.5",
-    "0.15.5+mixin.0.8.7",
-    "0.17.4+mixin.0.8.7",
-)
+val Project.FABRIC_MIXIN_VERSIONS
+    get() = listOf(
+        "0.10.7+mixin.0.8.4",
+        "0.13.4+mixin.0.8.5",
+        "0.15.5+mixin.0.8.7",
+        "0.17.4+mixin.0.8.7",
+    ) + listOf(LOCAL_FABRIC_MIXIN_VERSION).filter { hasProperty("fabricMixinDir") }
 
-val MIXINEXTRAS_VERSIONS = listOf(
-    "0.5.5",
-)
+val Project.MIXINEXTRAS_VERSIONS
+    get() = listOf(
+        "0.5.5",
+    ) + listOf(LOCAL_MIXINEXTRAS_VERSION).filter { hasProperty("mixinExtrasDir") }
 
 val MIXIN_REQUIRED_MIXINEXTRAS_BUMPS = emptyMap<String, String>()
 
