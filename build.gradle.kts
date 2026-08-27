8767dd4ac9b34a48adbedc47683b3895dd4a3ec0
LlamaLad7 <l3gomindstorms@gmail.com>
2026-08-27 17:50:24 +0100

    Build: Compile against local versions if applicable.

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,17 +53,9 @@ object Props {
 buildConfig {
     useJavaOutput()
     forClass(packageName = "com.llamalad7.mixintests.harness", className = "MixinArtifacts") {
-        val fabricMixinVersions = FABRIC_MIXIN_VERSIONS.toMutableList()
-        val mixinExtrasVersions = MIXINEXTRAS_VERSIONS.toMutableList()
-        if (hasProperty("fabricMixinDir")) {
-            fabricMixinVersions.add(LOCAL_FABRIC_MIXIN_VERSION)
-        }
-        if (hasProperty("mixinExtrasDir")) {
-            mixinExtrasVersions.add(LOCAL_MIXINEXTRAS_VERSION)
-        }
         buildConfigField("MIXIN_JARS", MIXIN_VERSIONS.associateWith(project::mixinJar))
-        buildConfigField("FABRIC_MIXIN_JARS", fabricMixinVersions.associateWith(project::fabricMixinJar))
-        buildConfigField("MIXINEXTRAS_JARS", mixinExtrasVersions.associateWith(project::mixinExtrasJar))
+        buildConfigField("FABRIC_MIXIN_JARS", FABRIC_MIXIN_VERSIONS.associateWith(project::fabricMixinJar))
+        buildConfigField("MIXINEXTRAS_JARS", MIXINEXTRAS_VERSIONS.associateWith(project::mixinExtrasJar))
         buildConfigField("MIXIN_REQUIRED_MIXINEXTRAS_BUMPS", MIXIN_REQUIRED_MIXINEXTRAS_BUMPS)
         buildConfigField("FABRIC_MIXIN_REQUIRED_MIXINEXTRAS_BUMPS", FABRIC_MIXIN_REQUIRED_MIXINEXTRAS_BUMPS)
     }
